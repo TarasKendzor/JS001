4410a272521efdefbda4492b4671057928b55e97
TarasKendzor <45481754+TarasKendzor@users.noreply.github.com>
2018-12-16 10:53:11 +0200

    update

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 
 
-const a = parseInt(prompt('Enter A'));
-const b = parseInt (prompt('Enter B'));
-const c = parseInt(prompt('Enter C'));
+const a = Numbers(prompt('Enter A'));
+const b = Numbers(prompt('Enter B'));
+const c = Numbers(prompt('Enter C'));
  
 
-function calculation (a ,b ,c){
+function quadraticEquation  (a ,b ,c){
 	const d = b* b - 4 *a *c ;
 	
 	if(d < 0){
-		alert('No real resuls because discriminant is under 0');
+		return('No real resuls because discriminant is under 0');
 	}
 	else if (d === 0 ){
 		const x = (-b)/(2 * a );
-		alert("This is your results"+ 'x=' + x );
+		return("This is your results"+ 'x=' + x );
 	}
 	else{
 		const dSqrt = Math.sqrt(d);
 		const x1 = (-b + dSqrt) / (2 * a);
 		const x2 = (- b - dSqrt) / (2 * a);
-		alert ( ' This is your results '+ 'x1='+ x1  + 'x2=' + x2);	
+		return( ' This is your results '+ 'x1='+ x1  + 'x2=' + x2);	
 	}
 	
 
 }
-var result = calculation (a,b ,c);
+
