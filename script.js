e20c7ac46b4070faf16a7ca6daba6c97d7258049
TarasKendzor <45481754+TarasKendzor@users.noreply.github.com>
2018-12-16 15:30:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 
 
-const a = Numbers(prompt('Enter A'));
-const b = Numbers(prompt('Enter B'));
-const c = Numbers(prompt('Enter C'));
- 
+const a = Number(prompt('Enter A'));
+const b = Number(prompt('Enter B'));
+const c = Number(prompt('Enter C'));
+const d = b* b - 4 *a *c ;
 
 function quadraticEquation  (a ,b ,c){
-	const d = b* b - 4 *a *c ;
 	
 	if(d < 0){
-		return('No real resuls because discriminant is under 0');
+		return 'No real resuls because discriminant is under 0';
 	}
 	else if (d === 0 ){
 		const x = (-b)/(2 * a );
-		return("This is your results"+ 'x=' + x );
+		return ' Your resuls: x= ' + x;
 	}
 	else{
 		const dSqrt = Math.sqrt(d);
 		const x1 = (-b + dSqrt) / (2 * a);
-		const x2 = (- b - dSqrt) / (2 * a);
-		return( ' This is your results '+ 'x1='+ x1  + 'x2=' + x2);	
+		const x2 = (-b - dSqrt) / (2 * a);
+		return 'Your resuls : x1= ' + x1 + ', x2= ' + x2;	
 	}
 	
 
-}
-
+} 
+alert(quadraticEquation(a, b, c));
